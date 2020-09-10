fcd80bd703f0d6a1048a498bdc45aee70374fbfb
test960 <57065037+test960@users.noreply.github.com>
2020-09-10 21:14:56 +0200

    Update Source.cpp
    2.0

--- Source.cpp
+++ Source.cpp
@@ -6,38 +6,12 @@ using namespace std;
 
 int main() {
 	Player* a = new Player("Maple", 150, 10);
-	Player* b = new Player("Sally", 45, 30);
+	Player* b = new Player("Sally", 45, 130);
 	Attack result;
 
 	a->print();
 	b->print();
-	/*
-	int bHealth = b->getHp();
-	int aHealth = a->getHp();
-	int i = 0;
-	while (a->getHp() > 0 && b->getHp() > 0){
-		if (i % 2 == 0) {
-			cout << a->getName() << " -> " << b->getName() << endl;
-			a->print();
-			bHealth -= a->getDmg();
-			if (bHealth < 0)
-				bHealth = 0;
-			b->setHp(bHealth);
-			b->print();
-		}
-		else {
-			cout << b->getName() << " -> " << a->getName() << endl;
-			aHealth -= b->getDmg();
-			if (aHealth < 0)
-				aHealth = 0;
-			a->setHp(aHealth);
-			a->print();
-			b->print();
-		}
-		i++;
-	}*/
-	result.atk(*a,*b);
-	//won
 	
+	result.atck(*a, *b);	
 	return 0;
 }
