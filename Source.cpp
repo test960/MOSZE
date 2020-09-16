171f2f887498a788a59eef5ab273080dfa235300
test960 <57065037+test960@users.noreply.github.com>
2020-09-16 21:25:18 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -5,9 +5,28 @@
 using namespace std; 
 
 int main() {
-	Player* a = new Player("Maple", 150, 10);
-	Player* b = new Player("Sally", 45, 130);
 	Attack result;
+	string name;
+	int hp, dmg;
+
+	//Első játékos feltöltése
+	cout << "Name: ";
+	cin >> name;
+	cout << "Hp: ";
+	cin >> hp;
+	cout << "Dmg: ";
+	cin >> dmg;
+	Player* a = new Player(name, hp, dmg);
+	
+	//Második játékos feltöltése
+	cout << "Name: ";
+	cin >> name;
+	cout << "Hp: ";
+	cin >> hp;
+	cout << "Dmg: ";
+	cin >> dmg;
+	Player* b = new Player(name, hp, dmg);
+
 
 	a->print();
 	b->print();
