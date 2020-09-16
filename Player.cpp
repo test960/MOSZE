71e99195e35c5701f151bd3fdb93a0d7f406e67a
test960 <57065037+test960@users.noreply.github.com>
2020-09-16 21:27:25 +0200

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -34,30 +34,3 @@ Player * Player::getKovetkezo() const{
 void Player::setKovetkezo(Player * kovetkezo){
 	this->kovetkezo = kovetkezo;
 }
-/*
-void Player::atk(){
-	Player* akt = playerList;
-	Player* kovetkezo = akt->getKovetkezo();
-	int healthA = akt->getHp();
-	int healthB = kovetkezo->getHp();
-
-	int i = 0;
-	while (akt->getHp() > 0 && kovetkezo->getHp() > 0) {
-		if (i % 2 == 0) {
-			cout << akt->getName() << " -> " << kovetkezo->getName() << endl;
-			cout << akt->getName() << ": Hp: " << akt->getHp() << ", Dmg: " << akt->getDmg() << endl;
-			healthB -= akt->getDmg();
-			kovetkezo->setHp(healthB);
-			cout << kovetkezo->getName() << ": Hp: " << kovetkezo->getHp() << ", Dmg: " << kovetkezo->getDmg() << endl;
-		}
-		else {
-			cout << kovetkezo->getName() << " -> " << akt->getName() << endl;
-			healthA -= kovetkezo->getDmg();
-			akt->setHp(healthA);
-			cout << akt->getName() << ": Hp: " << akt->getHp() << ", Dmg: " << akt->getDmg() << endl;
-			cout << kovetkezo->getName() << ": Hp: " << kovetkezo->getHp() << ", Dmg: " << kovetkezo->getDmg() << endl;
-		}
-		i++;
-	}
-}
-*/
