e8aa5f1a52281d58f15f571f85067610e24e2136
test960 <57065037+test960@users.noreply.github.com>
2020-09-24 16:52:49 +0200

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -3,11 +3,7 @@
 Player::Player(string name, int hp, int dmg) : name(name), hp(hp), dmg(dmg) {
 }
 
-Player::~Player()
-{
-}
-
-string Player::getName() const{
+string Player::getName() const {
 	return name;
 }
 
@@ -15,22 +11,14 @@ int Player::getHp() const {
 	return hp;
 }
 
-int Player::getDmg() const{
+int Player::getDmg() const {
 	return dmg;
 }
 
-void Player::print() const{
+void Player::print() const {
 	cout << getName() << ": HP: " << getHp() << ", Dmg: " << getDmg() << endl;
 }
-
-void Player::setHp(int h){
-	hp = h;
-}
-
-Player * Player::getKovetkezo() const{
-	return kovetkezo;
-}
-
-void Player::setKovetkezo(Player * kovetkezo){
-	this->kovetkezo = kovetkezo;
-}
+//
+//void Player::setHp(int h) {
+//	hp = h;
+//}
