15ab0fca92c2401fffc578a272e1ab73a69dcdea
mikroelek <57196182+mikroelek@users.noreply.github.com>
2020-09-24 18:40:57 +0200

    Update proba.cpp

--- elso/proba.cpp
+++ elso/proba.cpp
@@ -6,21 +6,17 @@ Player Player::parsePlayer(std::string fileName)
 		std::string name;
 		std::string temp_hp;
 		std::string temp_dmg;
-
 		input_file.ignore(1, '{');
 		input_file.ignore(1, '\n');
 		input_file.ignore(1, '\t');
-
 		std::getline(input_file, name, ',');
 		name.erase(std::remove(name.begin(), name.end(), '\"'), name.end());
 		name.erase(0, 7);
-
 		input_file.ignore(1, '\n');
 		input_file.ignore(1, '\t');
 		std::getline(input_file, temp_hp, ',');
 		temp_hp.erase(std::remove(temp_hp.begin(), temp_hp.end(), '\"'), temp_hp.end());
 		temp_hp.erase(0, 5);
-
 		input_file.ignore(1, '\n');
 		input_file.ignore(1, '\t');
 		std::getline(input_file, temp_dmg);
